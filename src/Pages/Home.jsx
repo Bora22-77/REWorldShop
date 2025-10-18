992153e6f2928a235c3214ef5b8072856a9c9b6c
Bora22-77 <boyra1took@gmail.com>
2025-10-18 16:06:25 +0700

    edit home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
-import New from '../Components/New/New'
-import Popular from '../Components/Popular/Popular'
+
 const Home = () => {
   return (
     <div>
