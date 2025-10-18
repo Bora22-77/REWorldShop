b99760f2ff6083ba31e3c9e97bcc7c1673da37d4
Bora22-77 <boyra1took@gmail.com>
2025-10-18 20:11:59 +0700

    add TestNew Folder

---
+++ src/Components/TestNew/TestNew.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './TestNew.css'
+const TestNew = () => {
+  return (
+    // <div>
+    //     <span>Bora</span>
+    // </div>
+    //---------------------------------------
+    // <div classNameName="fade-container">
+    //   <div classNameName="box">Welcome Bora!</div>
+    // </div>
+    //---------------------------------------
+    // <div className="box fade-in">Hello</div>
+     //---------------------------------------
+    // <div>
+    //     <button className="pulse">Click me</button>
+    // </div>
+     //---------------------------------------
+    // <div class="spinner" aria-hidden="true"></div>
+     //---------------------------------------
+    // <div class="stage">
+    //     <div class="ball"></div>
+    // </div>
+    //---------------------------------------
+    <div class="typewriter"><span>Welcome to my portfolio</span></div>
+
+  )
+}
+
+export default TestNew
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import TestNew from '../Components/TestNew/TestNew'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <TestNew/>
       {/* <New/>
       <Popular/> */}
     </div>
